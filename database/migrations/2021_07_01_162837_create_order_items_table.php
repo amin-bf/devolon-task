894c5712cbd025abf4e2c949a27fdcc44994fb71
Amin Bakhtiari Far <amin.bakhtiarifar@gmail.com>
2021-07-02 21:23:58 +0430

    Add orders
      - Order model
      - Order database migration
      - Order database seeder
    - OrderItem model
    - OrderItem database migration
    - OrderItem database seeder

---
+++ database/migrations/2021_07_01_162837_create_order_items_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->string("order_id");
+            $table->string("product_id");
+            $table->integer("quantity")->default(1);
+            $table->integer("amount")->default(0);
+
+            // Create constraint to product id
+            $table->foreign("product_id")
+                ->references("id")
+                ->on("products")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+
+            // Create constraint to order id
+            $table->foreign("order_id")
+                ->references("id")
+                ->on("orders")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
