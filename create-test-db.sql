e3c85e09fd8086fb9b0dcd22441f4351fabe9af9
Amin Bakhtiari Far <amin.bakhtiarifar@gmail.com>
2021-07-04 23:59:04 +0430

    Fix typo in test database creation file

--- create-test-db.sql
+++ create-test-db.sql
@@ -1,4 +1,6 @@
-# Create devolod_db_test database if it doesn`t exist
-CREATE DATABASE IF NOT EXISTS devolod_db_test;
-# Grant all privilidges on devolod_db_test to devolon_dbu
-GRANT ALL PRIVILEGES ON devolod_db_test.* TO 'devolon_dbu' identified by 'password';
+# Create devolon_db_test database if it doesn`t exist
+CREATE DATABASE IF NOT EXISTS devolon_db_test;
+# Grant all privilidges on devolon_db_test to devolon_dbu
+GRANT ALL PRIVILEGES ON devolon_db_test.* TO 'devolon_dbu'@'%';
+FLUSH PRIVILEGES;
+
